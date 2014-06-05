496866ec2e92c170f7156ac96f749ad784fbde22
chdtcktt <chdtcktt@gmail.com>
2014-06-04 19:34:06 -0700

    refactor

--- includes/functions.php
+++ includes/functions.php
@@ -5,3 +5,26 @@ function ConfirmQuery($result) {
         die("db query failed!");
     }
 }
+
+
+function FindAllSubjects() {
+    
+    //perform db query
+    global $con;
+    $query = "select * from subjects where visible = 1 order by position ASC";
+    $result = mysqli_query($con, $query);
+    ConfirmQuery($result);
+
+    return $result;
+}
+
+
+function FindPagesForSubject($subjectId) {
+
+    global $con;
+    $query = "select * from pages where visible = 1 and subject_id = {$subjectId} order by position ASC";
+    $pageSet = mysqli_query($con, $query);
+    ConfirmQuery($pageSet);
+    
+    return $pageSet;
+}
--- public/managecontent.php
+++ public/managecontent.php
@@ -1,62 +1,48 @@
-<?php 
+<?php
 $title = 'Manage Content';
 require_once '../includes/dbconnection.php';
 require_once '../includes/functions.php';
 include '../includes/layouts/top.php';
 ?>
 
-    
+
 <h2>Manage Content</h2>    
 
-<?php
-//perform db query
-$query = "select * from subjects where visible = 1 order by position ASC";
-$result = mysqli_query($con, $query);
-ConfirmQuery($result);
-
-?>
+<?php $subjectSet = FindAllSubjects(); ?>
 <nav>
     <ul class="list-group">
         <?php
         //user returned data
-        foreach ($result as $subject) {
+        foreach ($subjectSet as $subject) {
             ?>
             <li class="list-group-item"><?php
-            
-            echo $subject["menu_name"] ." (".
-                    $subject["id"]. ")";
-            
-            ?>
-                    <?php 
-                    $query = "select * from pages where visible = 1 and subject_id = {$subject["id"]} order by position ASC";
-                    $pageSet = mysqli_query($con, $query);
-                    ConfirmQuery($pageSet);
-                    
-                    ?>
+                echo $subject["menu_name"] . " (" .
+                $subject["id"] . ")";
+                ?>
+                <?php
+                $pageSet = FindPagesForSubject($subject["id"]);
+                ?>
                 <ul class="list-group">
-                    <?php
-                    foreach ($pageSet as $item) {
+                <?php
+                foreach ($pageSet as $item) {
                     ?>
-                    <li class="list-group-item">
-                        <?php 
+                        <li class="list-group-item">
+                        <?php
                         echo $item["menu_name"];
                         ?>
 
-                    </li>
-                    <?php }?>
+                        </li>
+    <?php } ?>
                 </ul>
             </li>
-<?php
-}
-?>
-
+<?php } ?>
     </ul>
 
 </nav>
 
+
 <?php
-        mysqli_free_result($result);
-?>
-<?php
+mysqli_free_result($result);
+mysqli_free_result($pageSet);
 include '../includes/layouts/bottom.php';
 
